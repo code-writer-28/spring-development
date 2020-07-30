3bcc93f10886c5d9646e547f194a5f4a8827c110
shivam74662 <shivam.thombre@active.ai>
2020-07-30 23:32:29 +0530

    spring development process, inversion of control by xml configuration file

---
+++ spring-demo-one/src/com/spring/CricketCoach.java
@@ -0,0 +1,10 @@
+package com.spring;
+
+public class CricketCoach implements Coach {
+
+	@Override
+	public String getDailyWorkOut() {
+		return "Spend 2 hrs on batting and 2 hrs on fielding and 2hrs on bowling";
+	}
+
+}
--- spring-demo-one/src/com/spring/HelloSpringApp.java
+++ spring-demo-one/src/com/spring/HelloSpringApp.java
@@ -12,7 +12,7 @@ public class HelloSpringApp {
 				"applicationContext.xml");
 
 		// retrieve bean from spring container
-		Coach coach = applicationContext.getBean("myCoach", Coach.class);
+		Coach coach = applicationContext.getBean("cricketCoach", Coach.class);
 
 		// call methods from the bean
 		System.out.println(coach.getDailyWorkOut());
@@ -23,3 +23,34 @@ public class HelloSpringApp {
 	}
 
 }
+
+/**
+ * FAQ: Why do we specify the Coach interface in getBean()?
+ * 
+ * Question
+ * 
+ * Why do we specify the Coach interface in getBean()?
+ * 
+ * For example:
+ * 
+ * Coach theCoach = context.getBean("myCoach", Coach.class);
+ * 
+ * ---
+ * 
+ * Answer
+ * 
+ * When we pass the interface to the method, behind the scenes Spring will cast
+ * the object for you.
+ * 
+ * context.getBean("myCoach", Coach.class)
+ * 
+ * However, there are some slight differences than normal casting.
+ * 
+ * From the Spring docs:
+ * 
+ * Behaves the same as getBean(String), but provides a measure of type safety by
+ * throwing a BeanNotOfRequiredTypeException if the bean is not of the required
+ * type. This means that ClassCastException can't be thrown on casting the
+ * result correctly, as can happen with getBean(String).
+ * 
+ */
