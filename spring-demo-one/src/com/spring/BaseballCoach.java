257f2549cbf94c89eefa459b9e61d45fc08267b3
shivam74662 <shivam.thombre@active.ai>
2020-07-30 20:23:25 +0530

    learning inversion of control

---
+++ spring-demo-one/src/com/spring/BaseballCoach.java
@@ -0,0 +1,10 @@
+package com.spring;
+
+public class BaseballCoach implements Coach {
+
+	@Override
+	public String getDailyWorkOut() {
+		return "spend 30 minutes on batting practice";
+	}
+
+}
---
+++ spring-demo-one/src/com/spring/TrackCoach.java
@@ -0,0 +1,10 @@
+package com.spring;
+
+public class TrackCoach implements Coach {
+
+	@Override
+	public String getDailyWorkOut() {
+		return "Run a Hard 5k";
+	}
+
+}
